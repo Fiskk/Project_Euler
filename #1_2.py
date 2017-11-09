96fb8cc16766f92382441a78a2a33b910a5ee78e
Fiskk <Fiskk@live.com>
2017-11-09 10:28:31 -0500

    Problem 1 answer

---
+++ #1_2.py
@@ -0,0 +1,69 @@
+#Steffan Sampson
+#11-8-2017
+#Project Euler Problem 1
+
+
+def sum_of_multiples():
+    print("This function finds the sums of multiples of integers below an upper bound")
+    print("This function takes in an upper bound")
+    print("As well as the numbers to be used to find the multiples")
+
+    #low_end = int(input("Please input your lower bound: "))
+    high_end = int(input("Please input your upper bound: "))
+
+    stop = False
+    ints = []
+    print("Please input values > 0 to be used to find multiples below the bound, to stop inputting input '-1'")
+    while stop is False:
+
+        value = int(input("Value: "))
+
+        if value > 0 and value % 1 == 0:
+            ints.append(value)
+
+        elif value == -1:
+            stop = True
+
+        else:
+            print("Input either an integer > 0 or -1")
+
+    print(ints)
+    print(str(high_end) + "\n")
+
+    num_of_values = len(ints) - 1
+
+    set_of_multiples = set()
+    n = 0
+    z = 1
+    while n <= num_of_values:
+
+        multiple = ints[n] * z
+
+        #print(multiple)
+        if multiple < high_end:
+            set_of_multiples.add(multiple)
+            z += 1
+        else:
+            n += 1
+            z = 1
+
+    #print(set_of_multiples)
+
+    num_of_multiples = len(set_of_multiples) - 1
+
+    #set_of_multiples = set(list_of_multiples)
+    #unique_list_of_multiples = [set_of_multiples]
+    total = 0
+    n = 0
+    while n <= num_of_multiples:
+        number = set_of_multiples.pop()
+        total += number
+
+        n += 1
+
+    print(total)
+
+
+sum_of_multiples()
+
+
